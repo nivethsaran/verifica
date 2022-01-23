d78ebc90521085c41bc4f70370acd34d3cc4677f
Niveth Saran <nivethsaran@gmail.com>
2022-01-23 13:55:50 +0530

    change properties

--- src/main/java/dev/niveth/verifica/VerificaApplication.java
+++ src/main/java/dev/niveth/verifica/VerificaApplication.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.DispatcherServlet;
 
 
 @SpringBootApplication
+@PropertySource("application-${spring.profiles.active}.properties")
 public class VerificaApplication {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(VerificaApplication.class);
@@ -17,7 +19,7 @@ public class VerificaApplication {
         ApplicationContext context = SpringApplication.run(VerificaApplication.class, args);
         DispatcherServlet dispatcherServlet = (DispatcherServlet) context.getBean("dispatcherServlet");
         dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-        LOGGER.info(System.getProperty("verifica.life"));
+        LOGGER.info(System.getProperty("spring.profiles.active"));
     }
 
 }
